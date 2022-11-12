026b7283dbb73d09dfb370061bbc7191d76b6fde
yaoshimax <yaoshimax@gmail.com>
2022-11-12 17:43:56 +0900

    ランダム選択と正解日の記録機能

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,22 @@ const _credentials = r'''
 
 const _spreadsheetId = '';
 
+// https://github.com/a-marenkov/gsheets/issues/31
+const double gsDateBase = 2209161600 / 86400;
+const double gsDateFactor = 86400000;
+
+double dateToGsheets(DateTime dateTime, {bool localTime = true}) {
+  final offset = dateTime.millisecondsSinceEpoch / gsDateFactor;
+  final shift = localTime ? dateTime.timeZoneOffset.inHours / 24 : 0;
+  return gsDateBase + offset + shift;
+}
+
+DateTime dateFromGsheets(String value, {bool localTime = true}) {
+  final date = double.parse(value);
+  final millis = (date - gsDateBase) * gsDateFactor;
+  return DateTime.fromMillisecondsSinceEpoch(millis.toInt(), isUtc: localTime);
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -62,27 +78,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  int _ind = 3;
-  String _question = "tes";
-  String _answer = "ans";
-
+  int _ind = -1;
   Future<Tuple2<String, String>> _getQandA() async {
     var ss = await widget.gsheets.spreadsheet(_spreadsheetId);
     var sheet = ss.worksheetByIndex(0);
-    var question = await sheet!.values.value(column: 1, row: _ind);
-    var answer = await sheet.values.value(column: 2, row: _ind);
+    var rows = await sheet!.values.allRows(fromRow: 2);
+    var size = rows.length;
+    var candidateIndex = <int>[];
+    var now = DateTime.now();
+    for (int i = 0; i < size; i++) {
+      if (i == _ind) continue;
+      print(rows[i][3]);
+      var currentDate = dateFromGsheets(rows[i][3]);
+      if (currentDate.isAfter(now)) {
+        candidateIndex.add(i);
+      }
+    }
+    if (candidateIndex.isEmpty) {
+      return Future.error("all not detected");
+    }
+    candidateIndex.shuffle();
+    _ind = candidateIndex[0];
+    var question = rows[_ind][0];
+    var answer = rows[_ind][1];
     return Future<Tuple2<String, String>>.value(Tuple2<String, String>(question, answer));
   }
 
-  void _incrementCounter() async {
+  void _ok(int ind) async {
+    var ss = await widget.gsheets.spreadsheet(_spreadsheetId);
+    var sheet = ss.worksheetByIndex(0);
+    var targetCell = await sheet!.cells.cell(column: 3, row: ind + 2);
+    var now = DateTime.now();
+    await targetCell.post(now);
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
@@ -97,73 +130,87 @@ class _MyHomePageState extends State<MyHomePage> {
     var _height = MediaQuery.of(context).size.height;
     var _width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FutureBuilder(
-                future: _getQandA(),
-                builder: (BuildContext context, AsyncSnapshot<Tuple2<String, String>> snapshot) {
-                  if (snapshot.connectionState != ConnectionState.done) {
-                    return const CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Text(snapshot.error.toString());
-                  } else if (snapshot.hasData) {
-                    return FlipCard(
-                        direction: FlipDirection.HORIZONTAL,
-                        front: Container(
-                            width: _width * 0.8,
-                            height: _height * 0.8,
-                            padding: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              color: Colors.white70,
-                              borderRadius: BorderRadius.circular(20),
-                              border: Border.all(width: 1),
-                            ),
-                            child: Text(snapshot.data!.item1, style: Theme.of(context).textTheme.headline4)),
-                        back: Container(
-                            width: _width * 0.8,
-                            height: _height * 0.8,
-                            padding: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                              color: Colors.white70,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(width: 1),
-                            ),
-                            child: Text(snapshot.data!.item2, style: Theme.of(context).textTheme.headline4)));
-                  } else {
-                    return const Text("データ取得に失敗しました");
-                  }
-                }),
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FutureBuilder(
+                  future: _getQandA(),
+                  builder: (BuildContext context, AsyncSnapshot<Tuple2<String, String>> snapshot) {
+                    if (snapshot.connectionState != ConnectionState.done) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text(snapshot.error.toString());
+                    } else if (snapshot.hasData) {
+                      return FlipCard(
+                          direction: FlipDirection.HORIZONTAL,
+                          front: Container(
+                              width: _width * 0.8,
+                              height: _height * 0.8,
+                              padding: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                color: Colors.white70,
+                                borderRadius: BorderRadius.circular(20),
+                                border: Border.all(width: 1),
+                              ),
+                              child: Text(snapshot.data!.item1, style: Theme.of(context).textTheme.headline5)),
+                          back: Container(
+                              width: _width * 0.8,
+                              height: _height * 0.8,
+                              padding: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                color: Colors.white70,
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(width: 1),
+                              ),
+                              child: Text(snapshot.data!.item2, style: Theme.of(context).textTheme.headline5)));
+                    } else {
+                      return const Text("データ取得に失敗しました");
+                    }
+                  }),
+            ],
+          ),
+        ),
+        floatingActionButton: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            FloatingActionButton(
+              heroTag: 'ng',
+              onPressed: () => _ok(_ind),
+              tooltip: 'Increment',
+              child: const Icon(Icons.done),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: 'ng',
+              onPressed: () => _ok(_ind),
+              tooltip: 'Increment',
+              child: const Icon(Icons.not_interested_sharp),
+            ),
+          ],
+        )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
